e83696d8bca1924292bc139bf8796c8e259cc4f0
Curtis Easton <curtis.easton@gmail.com>
2020-05-03 17:29:12 -0700

    IO update

--- labs/08_file_io/08_02_writing.py
+++ labs/08_file_io/08_02_writing.py
@@ -7,14 +7,6 @@ with  open('words.txt', 'r') as fin:
     text = (fin.readlines())  # read schoology .csv into python
 
 
-
-with open("words_reverse.txt", "w") as fout:
-    for item in text:
-            fout.write(text[x])
-        
-
-
-
 with open("words_reverse.txt", "w") as fout:
     for x in reversed(range(len(text))):
             fout.write(text[x])
