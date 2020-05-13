76c420262176b11e41ae22b6cead61ee57a99ef2
Curtis Easton <curtis.easton@gmail.com>
2020-05-12 20:24:51 -0700

    generators

--- labs/15_generators/15_02_divisible.py
+++ labs/15_generators/15_02_divisible.py
@@ -3,3 +3,8 @@ Create a Generator that loops over the given list and prints out only
 the items that are divisible by 1111.
 
 '''
+list_ = [0, 1111, 2222, 4, 69, 9999, 19998]
+
+gen = (x for x in list_ if x%1111==0)
+
+print([item for item in gen])
--- labs/15_generators/15_03_floor.py
+++ labs/15_generators/15_03_floor.py
@@ -4,3 +4,11 @@ Adapt your Generator expression from the previous Exercise
 What numbers do you get?
 
 '''
+list_ = [0, 1111, 2222, 4, 69, 9999, 19998]
+
+gen = (x for x in list_ if x%1111==0)
+
+print([item//1111 for item in gen])
+
+
+# I get the number of times 1111 goes into each number in gen
