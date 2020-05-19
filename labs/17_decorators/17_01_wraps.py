c32804a40e7d14a31f692c0cf7b72637fbda6cd5
Curtis Easton <curtis.easton@gmail.com>
2020-05-18 21:48:40 -0700

    decorators

--- labs/17_decorators/17_01_wraps.py
+++ labs/17_decorators/17_01_wraps.py
@@ -2,3 +2,16 @@
 Write a decorator function that wraps text passed to it in a html <p> tag.
 
 '''
+def decorate(func):
+    def wrapper(item):
+        return "<p>"+ func(item) + "</p>"
+    return wrapper
+
+
+@decorate
+def shout(msg):
+    return msg
+
+
+a=shout("message")
+print(a)
--- labs/17_decorators/17_02_tags.py
+++ labs/17_decorators/17_02_tags.py
@@ -3,3 +3,17 @@ Improve the decorator from the previous exercise by allowing it to take
 any specified HTML tag as an input - making it more general.
 
 '''
+
+def decorate(func):
+    def wrapper(tag,item):
+        return tag+ func(item) + tag[0] + "/" + tag[1:]
+    return wrapper
+
+
+@decorate
+def shout(msg):
+    return msg
+
+
+a=shout('<m>',"message")
+print(a)
