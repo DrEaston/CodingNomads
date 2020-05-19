23142b52c8a518afd7cced4511923f1de44258f9
Curtis Easton <curtis.easton@gmail.com>
2020-05-18 19:15:37 -0700

    args and kwargs

--- labs/16_variable_arguments/16_01_args.py
+++ labs/16_variable_arguments/16_01_args.py
@@ -2,3 +2,10 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+things_i_like=['pizza','piano','butts']
+
+def shout(*likes):
+    for pleasure in things_i_like:
+        print(pleasure)
+        
+shout(things_i_like)
--- labs/16_variable_arguments/16_02_kwargs.py
+++ labs/16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,11 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+things_i_like={'like1':'pizza','like2':'piano','like3':'butts'}
+
+def shout(**likes):
+    for index, pleasure in things_i_like.items():
+        print(index, pleasure)
+        
+shout(**things_i_like)
